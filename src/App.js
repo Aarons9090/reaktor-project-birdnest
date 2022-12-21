4b9b94dcc906f2e6182b5f63d51e034313f9886f
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-12-21 21:04:46 +0200

    add xml parsing and drone distance calculation

--- src/App.js
+++ src/App.js
@@ -1,45 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import droneService from './services/drones';
+import logo from "./logo.svg"
+import "./App.css"
+import React, { useState, useEffect } from "react"
+import droneService from "./services/drones"
 
-function App() {
-  const [drones, setDrones] = useState([]);
+const calculateDistance = (x1,y1,x2,y2) => {
 
-  useEffect(() => {
-    const fetchDrones = async () => {
-      const resp = await droneService.getAllDrones();
-      setDrones(resp);
-    };
-    fetchDrones();
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(drones,"text/xml");
 
-    console.log(typeof(xmlDoc));
-    console.log(xmlDoc);
-    const drones_xml = xmlDoc.getElementsByTagName("drone");
-    console.log(drones_xml);
-
-  }, [drones]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+    console.log("Distance: " + dist)
+    return dist
 }
 
-export default App;
+const calculateDistanceToAllDrones = drones => {
+    const violatedDrones = []
+    for (let i = 0; i < drones.length; i++) {
+      const drone = drones[i]
+      if(calculateDistance(parseInt(drone.positionX), parseInt(drone.positionY), 250000, 250000) < 100000) {
+        violatedDrones.push(drone)
+      }
+    }
+    return violatedDrones
+}
+
+
+function App() {
+    const [drones, setDrones] = useState([])
+
+    // fetch drones every 2 seconds
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const fetchDrones = async () => {
+                const resp = await droneService.getAllDrones()
+                console.log("Drones:")
+                console.log(resp)
+                setDrones(resp)
+            }
+            fetchDrones()
+        }, 2 * 1000)
+        return () => clearInterval(interval)
+    }, [drones])
+
+    useEffect(() => {
+        const violatedDrones = calculateDistanceToAllDrones(drones)
+        console.log("Violated drones:")
+        console.log(violatedDrones)
+    }, [drones])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
+}
+
+export default App
--- src/services/drones.js
+++ src/services/drones.js
@@ -1,29 +1,39 @@
 import axios from "axios";
 const URL = "/birdnest/drones";
 
+const getStringFromXml = (xmlDoc, tagName) => xmlDoc.getElementsByTagName(tagName)[0].textContent
+
 const getAllDrones = async () => {
     const response = await axios.get(URL);
 
-    // parse xml data to object
-
+    // parse xml data to HTMLObj
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(response.data,"text/xml");
 
-    console.log(typeof(xmlDoc));
-    console.log(xmlDoc);
     const drones_xml = xmlDoc.getElementsByTagName("drone");
-    console.log(drones_xml);
     let drones = [];
+
+    if(drones_xml.length === 0){
+        return drones;
+    }
 
     for(let i = 0; i < drones_xml.length; i++){
         const drone = {
-            model: drones_xml[0].getElementsByTagName("model")[0].textContent,
+            model: getStringFromXml(drones_xml[i], "model"),
+            serialNumber: getStringFromXml(drones_xml[i], "serialNumber"),
+            manufacturer: getStringFromXml(drones_xml[i], "manufacturer"),
+            mac: getStringFromXml(drones_xml[i], "mac"),
+            ipv4: getStringFromXml(drones_xml[i], "ipv4"),
+            ipv6: getStringFromXml(drones_xml[i], "ipv6"),
+            firmware: getStringFromXml(drones_xml[i], "firmware"),
+            positionY: getStringFromXml(drones_xml[i], "positionY"),
+            positionX: getStringFromXml(drones_xml[i], "positionX"),
+            altitude: getStringFromXml(drones_xml[i], "altitude"),
         }
         drones.push(drone);
     }
-    console.log(drones)
 
-    return response.data;
+    return drones;
 }
 
 export default { getAllDrones };
